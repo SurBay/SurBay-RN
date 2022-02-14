65722818edc900e32dba1a923495bbb53edfbc7b
cryingInitial <specific0924@gmail.com>
2022-02-14 14:41:23 +0900

    디렉토리 추가 및 alias 등록

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@context': './src/context',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@res': './src/res',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@styles': './src/styles',
+        },
+      },
+    ],
+  ],
 };
